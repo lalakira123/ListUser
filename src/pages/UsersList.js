1b8c5e046225256a82920c18d76dfe9e52058ce1
Laerte <laerteakira@gmail.com>
2022-09-20 10:31:18 -0300

    feat: filters per status and search by name

--- src/pages/UsersList.js
+++ src/pages/UsersList.js
@@ -7,29 +7,44 @@ import styled from "styled-components";
 
 export default function UsersList() {
   const [users, setUsers] = useState([]);
-  const [emptyFilter, setEmptyFilter] = useState(0);
+  const [data, setData] = useState([]);
+  const [searchName, setSearchName] = useState('');
 
   useEffect(() => {
     const promise = axios.get('https://gorest.co.in/public/v2/users');
     promise.then((response) => {
       const { data } = response;
       setUsers(data);
+      setData(data);
     });
     promise.catch((e) => {
       console.log(e);
     });
-  }, [emptyFilter]);
+  }, []);
 
   function filterStatusHandler(status){
     const filterArray = [];
-    if(users.length > 0){
-      for(let i = 0; i < users.length; i++){
-        if( status === users[i].status ){
-          filterArray.push(users[i]);
+    if(data.length > 0){
+      for(let i = 0; i < data.length; i++){
+        if( status === data[i].status ){
+          filterArray.push(data[i]);
         }
       }
       setUsers(filterArray);
     }
+  }
+  
+  function findNameHandler(state){
+    const searchedArray = [];
+    if(data.length > 0 && state !== ''){
+      for(let i = 0; i < data.length; i++){
+        if( state === data[i].name ){
+          searchedArray.push(data[i]);
+          break;
+        }
+      }
+      setUsers(searchedArray);
+    } 
   }
 
   return(
@@ -39,21 +54,33 @@ export default function UsersList() {
         <h2>FILTROS: </h2>
         <p onClick={() => filterStatusHandler('active')}>Active</p>
         <p onClick={() => filterStatusHandler('inactive')}>Inactive</p>
-        <p onClick={() => setEmptyFilter(emptyFilter + 1)}>Limpar Filtro</p>
+        <p onClick={() => setUsers(data)}>Limpar Filtro</p>
+      </div>
+      <div className="filters">
+        <h2>BUSCA: </h2>
+        <input 
+          placeholder="Pesquisar nome"
+          value={searchName}
+          onChange={(e) => setSearchName(e.target.value)}
+        />
+        <p onClick={() => findNameHandler(searchName)}>Buscar</p>
       </div>
       <PageContainer>
         {
-          users.map((item) => {
-            const { name, email, gender, status } = item;
-            return(
-              <BoxUser 
-                name={name}
-                email={email}
-                gender={gender}
-                status={status}
-              />
-            );
-          })
+          users.length > 0 ?
+            users.map((item) => {
+              const { name, email, gender, status } = item;
+              return(
+                <BoxUser 
+                  name={name}
+                  email={email}
+                  gender={gender}
+                  status={status}
+                />
+              );
+            })
+            :
+            <p>Não encontrado. Limpe o filtro.</p>
         }
       </PageContainer>
     </Page>
@@ -71,12 +98,19 @@ const Page = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    width: 400px;
+    width: 370px;
     margin-top: 20px;
     p {
       font-weight: 500;
       background-color: #ffffff;
       border-radius: 20px;
+      padding: 10px;
+    }
+    input {
+      width: 200px;
+      height: 30px;
+      border-radius: 20px;
+      border: none;
       padding: 10px;
     }
   }
